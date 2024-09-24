42b7b515c227edb9d93e9a9079cf367eee7aa13d
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-09-24 16:52:38 -0400

    Added Database connection with ORM

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/healthz', (req, res) => {
+//Database connection via Sequelize
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
+  });
+
+app.get('/healthz', async (req, res) => {
     try{
+        await sequelize.authenticate();
         res.sendStatus(200);
     }catch(err){
         res.sendStatus(503);
