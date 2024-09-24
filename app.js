7d99823a0455cc2744e147b6f2f6a26d00392cb9
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-09-24 16:46:36 -0400

    added health check endpoint

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/healthz', (req, res) => {
+    try{
+        res.sendStatus(200);
+    }catch(err){
+        res.sendStatus(503);
+    }
+});
+
+app.all('*', (req, res) => {
+    res.sendStatus(405);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
