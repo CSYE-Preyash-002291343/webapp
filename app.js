b56c857d7c6ab67ce2245ac86006e30b0d79c93f
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-10-01 18:24:40 -0400

    Moved healthz to route folder and also added Initial DB connection method

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require("dotenv").config();
+const healthzRouter = require('./routes/healthz');
+const User = require('./models/userModel.js');
 
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -14,56 +16,18 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     port: process.env.DB_PORT
   });
 
-//Payload method to check if request body is empty
-function payload (req, res, next) {
-    if(Object.keys(req.body).length !== 0 || Object.keys(req.query).length !== 0){
-        res.status(400).send();       
-    }
-    if(req.headers && req.headers["content-length"] > 0){
-        res.status(400).send();
-    }
-    if(req.url.includes('?')){
-         res.status(400).send();
-    }
-    next();
-}
-
-//Error handling middleware to not show client error messages in body
-app.use((err, req, res, next) => {
-    if(err.status === 400){
-        res.status(400).send();
-    }
-    next();
-});
-
-//healthz endpoint
-app.get('/healthz', payload, async (req, res) => {
+//Check if database connection is successful
+async function dbconnect(){
     try{
         await sequelize.authenticate();
-        res.header('Cache-Control', 'no-store');
-        res.header('Pragma', 'no-cache');
-        res.header('Expires', '0');
-        res.status(200).send();
+        console.log('Connected to DB');
     }catch(err){
-        res.status(503).send();
+        console.error('Disconnected from DB');
     }
-});
+}
+dbconnect();
 
-//405 method not allowed for any other endpoint
-app.all('/healthz',  (req, res) => {
-    res.header('Cache-Control', 'no-store');
-    res.header('Pragma', 'no-cache');
-    res.header('Expires', '0');
-    res.status(405).send();
-});
-
-//404 not found for wrong endpoints
-app.all('*', (req, res) => {
-    res.header('Cache-Control', 'no-store');
-    res.header('Pragma', 'no-cache');
-    res.header('Expires', '0');
-    res.status(404).send();
-});
+app.use('/healthz', healthzRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
