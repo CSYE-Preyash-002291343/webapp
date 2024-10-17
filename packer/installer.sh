0c59629a4b905301b31c037586580739d2b680b1
Preyash Amar Mehta <mehta.prey@northeastern.edu>
2024-10-16 23:32:44 -0400

    changed postgres installer setup

--- packer/installer.sh
+++ packer/installer.sh
@@ -5,9 +5,7 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # Install PostgreSQL
-echo "Installing PostgreSQL 16.4..."
-wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /usr/share/keyrings/postgresql-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/postgresql-archive-keyring.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+echo "Installing PostgreSQL .."
 sudo apt update
 sudo apt install -y postgresql postgresql-contrib
 
